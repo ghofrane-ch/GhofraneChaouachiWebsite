fb1be77f9b2a34222710f7e2bc05ed1e9e904010
Ghofrane CHAOUACHI <chaouachi.ghofran@gmail.com>
2024-09-11 15:34:44 -0400

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,205 @@
+const translations = {
+    "en": {
+        "home": "Home",
+        "aboutMe": "About Me",
+        "experience": "Experience",
+        "education": "Education",
+        "certifications": "Certifications",
+        "projects": "Projects",
+        "hobbies": "Hobbies",
+        "skills": "Skills",
+        "welcomeMessage": "Hello there, I am Ghofrane CHAOUACHI",
+        "titre": "A Bilingual Computer Programming Student at Collège la Cité (Ottawa Campus)",
+        "contact": "Hire Me",
+        "downloadResume": "Download Resume",
+        "descriptionAboutMe1": "With a strong foundation in banking and finance, I combine analytical prowess with advanced programming skills to tackle complex challenges. ",
+        "descriptionAboutMe2": "Over nine years of experience in credit analysis, financial management, and project coordination have sharpened my ability to drive operational efficiency and deliver results. ",
+        "descriptionAboutMe3": "Bilingual in English and French, I excel in building meaningful relationships with diverse teams and clients. ",
+        "descriptionAboutMe4": "My passion for continuous learning fuels my commitment to leveraging technology and financial expertise to make impactful contributions.",
+        "programmingLanguages": "Programming Languages",
+        "toolsTechnologies": "Tools & Technologies",
+
+        "toolsTechnologies2": "Microsoft Office Suite (Excel, Word, PowerPoint, Outlook)",
+        "toolsTechnologies1": "Git (branching, merging, and collaboration)",
+        "toolsTechnologies3":"Data Analysis & Visualization (Financial risk assessment, data management)",
+        "toolsTechnologies5": "Blockchain Essentials",
+        "toolsTechnologies4": "Artificial Intelligence Fundamentals",
+        "projectManagement":"Project & Communication Skills",
+        "projectManagement1":"Strong documentation practices",
+        "projectManagement2":"Attention to detail in project management",
+    
+
+        "showTasks": 'Show Tasks',
+        "hideTasks": 'Hide Tasks',
+
+        "placeTunis": "Tunis, Tunisia",
+        "placeOttawa": "Ottawa, Canada",
+        "creditAnalyst": "Credit Analyst",
+        "placeCredit": "Bank of Tunisia and the Emirates",
+        "dateCredit": "June 2015 – April 2023",
+        "tasksCredit1": "Analyzed and prepared periodic leasing activity reports to identify departmental performance improvements.",
+        "tasksCredit2": "Assisted in developing and testing an information system with the IT department, ensuring accurate data migration.",
+        "tasksCredit3": "Developed and implemented an Excel-based system that reduced credit application response times from 3-5 days to 1-2 days, significantly optimizing departmental efficiency.",
+        "tasksCredit4": "Evaluated and processed credit files, ensuring comprehensive financial assessments for the Decision Committee.",
+        "tasksCredit5": "Coordinated with teams to ensure timely delivery of approved financing documents.",
+
+        "financialAssistant": "Financial Assistant",
+        "dateFinancialAssistantMib": "February 2015 – May 2015",
+        "placeFinancialAssistantMib": "MIB (Slama Group subsidiary)",
+        "tasksFinancialAssistantMib1": "Managed clearance of trade titles and import authorizations for foreign and local suppliers.",
+        "tasksFinancialAssistantMib2": "Prepared provisional financial statements and managed budgetary commitments.",
+
+        "assistantBank": "Assistant of the Bank Key Account Manager",
+        "dateAssistantBank": "August 2014 – October 2014",
+        "placeAssistantBank": "Attijari Leasing (Attijari Bank sub.)",
+        "tasksAssistantBank1": "Supported the Key Account Manager in daily tasks, including analyzing leasing financing requests.",
+        "tasksAssistantBank2": "Coordinated document preparation and submission to internal committees, ensuring timely processing.",
+
+        "financialAssistant": "Financial Assistant",
+        "dateFinancialAssistantSame": "June 2014 – August 2014",
+        "placeFinancialAssistantSame": "SAME Hydraulic",
+        "tasksFinancialAssistantSame1": "Prepared payments for foreign and local suppliers and negotiated preferential banking conditions.",
+        "tasksFinancialAssistantSame2": "Provided administrative support to the CFO, managing financial commitments and documentation.",
+
+        "degree1": "College Diploma in Computer Programming (COOP mode)",
+        "dateDegree1": "September 2023 – April 2026 (Expected)",
+        "schoolDegree1": "Collège la Cité (Ottawa Campus)",
+
+        "degree2": "Master’s Degree in International Business Affairs",
+        "dateDegree2": "September 2017 - June 2019",
+        "schoolDegree2": "Faculty of Economic Science and Management of Tunis",
+
+        "degree3": "Bachelor’s Degree in Economics (Specialized in Currency, Finance, and Banking)",
+        "dateDegree3": "September 2011 - June 2014",
+        "schoolDegree3": "Faculty of Economic Science and Management of Tunis",
+
+        "certification1": "Artificial Intelligence Fundamentals | SkillUp 2024 – IBM SkillsBuild | August 2024",
+        "certification2": "Microsoft SQL – Udemy | February 2024",
+        "certification3": "Blockchain Essentials V2 – IBM | December 2022",
+        "certification4": "Financing Small and Medium Companies and Risk Management – USAID | April 2019",
+        "certification5": "Advanced EXCEL – TFC FORMATION | October 2017",
+        "certification6": "Reading and Analysis of Financial Statements – MW CONSULTING | September 2016",
+
+        "project1Title": "Personal Website",
+        "project1Description": "Developed a multilingual personal website using HTML, CSS, JavaScript.",
+        "project2Title": "Excel With Me",
+        "project2Description": "Creating videos to share valuable tips and tricks about Excel on LinkedIn. Here are the links to each episode:",
+        "project3Description": "Developed during my first year of college, 'Wanderlust Log' is an Android application designed to help travelers plan their adventures with ease and efficiency.",
+        "hobby1": "Traveling the Globe: I have a passion for discovering new cultures and hidden gems around the world. Each journey not only broadens my perspective but also fuels my creativity and problem-solving skills.",
+        "hobby2": "Travel Planning: I take pride in meticulously planning trips for family and friends, tailoring each itinerary to their unique preferences. From finding the best local eateries to organizing adventure-packed days, I enjoy making every trip a memorable experience. My attention to detail and organizational skills translate seamlessly into my professional life, where I manage projects with the same precision and care."
+    },
+    "fr": {
+        "home": "Accueil",
+        "aboutMe": "À propos de moi",
+        "experience": "Expérience",
+        "education": "Éducation",
+        "certifications": "Certifications",
+        "projects": "Projets",
+        "hobbies": "Loisirs",
+        "skills": "Compétences",
+        "welcomeMessage": "Bonjour, je suis Ghofrane CHAOUACHI",
+        "titre": "Étudiante bilingue en programmation informatique a Collège la Cité (Campus Ottawa)",
+        "contact": "Engagez-moi",
+        "downloadResume": "Télécharger CV (English Version)",
+        "descriptionAboutMe1": "Avec une solide formation en banque et finance, je combine une expertise analytique avec des compétences avancées en programmation pour relever des défis complexes.",
+        "descriptionAboutMe2": "Plus de neuf ans d'expérience en analyse de crédit, gestion financière et coordination de projets ont affiné ma capacité à améliorer l'efficacité opérationnelle et à obtenir des résultats. ",
+        "descriptionAboutMe3": "Bilingue en anglais et en français, j'excelle dans l'établissement de relations significatives avec des équipes et des clients divers. ",
+        "descriptionAboutMe4": "Ma passion pour l'apprentissage continu nourrit mon engagement à utiliser la technologie et l'expertise financière pour apporter des contributions significatives.",
+
+        "programmingLanguages": "Langages de programmation",
+        "toolsTechnologies": "Outils & Technologies",
+
+        "toolsTechnologies2": "Suite Microsoft Office (Excel, Word, PowerPoint, Outlook)",
+        "toolsTechnologies1": "Git (création de branches, fusion et collaboration)",
+        "toolsTechnologies3":"Analyse et visualisation des données (Évaluation des risques financiers, gestion des données)",
+        "toolsTechnologies5": "Les essentiels du Blockchain",
+        "toolsTechnologies4": "Les fondamentaux de l'intelligence artificielle",
+
+        "projectManagement":"Compétences en gestion de projet et en communication",
+        "projectManagement1":"Pratiques rigoureuses de documentation",
+        "projectManagement2":"Soucieuse du détail dans la gestion de projet",
+
+        "showTasks": "Afficher les tâches",
+        "hideTasks": "Masquer les tâches",
+
+        "placeTunis": "Tunis, Tunisie",
+        "placeOttawa": "Ottawa, Canada",
+        "creditAnalyst": "Analyste de Crédit",
+        "placeCredit": "Banque de Tunisie et des Emirats",
+        "dateCredit": "Juin 2015 – Avril 2023",
+        "tasksCredit1": "Analyse et préparation de rapports périodiques sur l'activité du departement pour identifier les améliorations des performances du département.",
+        "tasksCredit2": "Assistance dans le développement et les tests d'un système d'information avec le département informatique, assurant une migration des données précise.",
+        "tasksCredit3": "Développement et mise en œuvre d'un système basé sur Excel réduisant les délais de réponse des demandes de crédit de 3 à 5 jours à 1 à 2 jours, et optimisant l'efficacité du département.",
+        "tasksCredit4": "Évaluation et traitement des dossiers de crédit, assurant des évaluations financières complètes pour le Comité de Décision.",
+        "tasksCredit5": "Coordination avec les équipes pour garantir la livraison en temps opportun des documents de financement approuvés.",
+
+        "financialAssistant": "Assistante Financière",
+        "placeFinancialAssistantMib": "MIB (Filiale Groupe Slama)",
+        "dateFinancialAssistantMib": "Février 2015 – Mai 2015",
+        "tasksFinancialAssistantMib1": "Gestion du dédouanement des titres de commerce et des autorisations d'importation pour les fournisseurs étrangers et locaux.",
+        "tasksFinancialAssistantMib2": "Préparation des états financiers provisoires et gestion des engagements budgétaires.",
+
+        "dateAssistantBank": "Aout 2014 – Octobre 2014",
+        "assistantBank": "Assistante du Responsable des Comptes Clés",
+        "placeAssistantBank": "Attijari Leasing (Filiale Attijari Bank)",
+        "tasksAssistantBank1": "Soutien au Responsable des Grands Comptes dans les tâches quotidiennes, y compris l'analyse des demandes de financement de leasing.",
+        "tasksAssistantBank2": "Coordination de la préparation et de la soumission des documents aux comités internes, garantissant un traitement rapide.",
+
+        "dateFinancialAssistantSame": "Juin 2014 – Aout 2014",
+        "placeFinancialAssistantSame": "SAME Hydraulique• Tunis, Tunisie",
+        "tasksFinancialAssistantSame1": "Préparation des paiements pour les fournisseurs étrangers et locaux et négociation des conditions bancaires préférentielles.",
+        "tasksFinancialAssistantSame2": "Fourniture de soutien administratif au CFO, gestion des engagements financiers et documentation.",
+
+        "degree1": "DEC en programmation informatique (en mode COOP)",
+        "dateDegree1": "Septembre 2023 – Avril 2026 (Prévu)",
+        "schoolDegree1": "Collège la Cité (Campus Ottawa)",
+
+        "degree2": "Master (Maitrise) en Affaires Commerciales Internationales",
+        "dateDegree2": "Septembre 2017 - Juin 2019",
+        "schoolDegree2": "Faculté des Sciences Economiques et de Gestion de Tunis",
+
+        "degree3": "Licence Appliquée (Baccalauréat) en Economie (Spécialisée en Monnaie, Finance et Banques)",
+        "dateDegree3": "Septembre 2011 - Juin 2014",
+        "schoolDegree3": "Faculté des Sciences Economiques et de Gestion de Tunis",
+
+        "certification1": "Fondamentaux de l'Intelligence Artificielle | SkillUp 2024 – IBM SkillsBuild | Août 2024",
+        "certification2": "Microsoft SQL – Udemy | Février 2024",
+        "certification3": "Blockchain Essentials V2 – IBM | Décembre 2022",
+        "certification4": "Financement des Petites et Moyennes Entreprises et Gestion des Risques – USAID | Avril 2019",
+        "certification5": "EXCEL Avancé – TFC FORMATION | Octobre 2017",
+        "certification6": "Lecture et Analyse des États Financiers – MW CONSULTING |Septembre 2016",
+
+        "project1Title": "Site Web Personnel",
+        "project1Description": "Développement d'un site web personnel multilingue en utilisant HTML, CSS, JavaScript.",
+        "project2Title": "Excel Avec moi",
+        "project2Description": "Création de vidéos pour partager des conseils et astuces précieux sur Excel sur LinkedIn. Voici les liens vers chaque épisode :",
+        "project3Description": "Développé lors de ma première année du collège, 'Wanderlust Log' est une application Android conçue pour aider les voyageurs à planifier leurs aventures avec facilité et efficacité.",
+
+        "hobby1": "Voyager à travers le monde : Que ce soit en explorant les rues animées d'Istanbul ou les paysages pittoresques de la Toscane, j'ai une passion pour la découverte de nouvelles cultures et de trésors cachés à travers le monde. Chaque voyage élargit non seulement ma perspective, mais nourrit également ma créativité et mes compétences en résolution de problèmes.",
+        "hobby2": "Planification de voyages : J'aime planifier méticuleusement des voyages pour ma famille et mes amis, en adaptant chaque itinéraire à leurs préférences uniques. De la recherche des meilleurs restaurants locaux à l'organisation de journées riches en aventures, j'apprécie de faire de chaque voyage une expérience inoubliable. Mon attention aux détails et mes compétences en organisation se traduisent parfaitement dans ma vie professionnelle, où je gère les projets avec la même précision et le même soin.",
+        "hobby3": "Cuisine"
+    }
+};
+
+let currentLanguage = 'en';
+function setLanguage(language) {
+    currentLanguage = language;
+    document.querySelectorAll('[data-translate]').forEach((element) => {
+        const key = element.getAttribute('data-translate');
+        if (translations[language][key]) {
+            element.textContent = translations[language][key];
+        }
+    });
+}
+
+function toggleTasks(button) {
+    const tasksContainer = button.nextElementSibling;
+    if (tasksContainer.style.display === 'block') {
+        tasksContainer.style.display = 'none';
+        button.innerText = translations[currentLanguage]["showTasks"];
+    } else {
+        tasksContainer.style.display = 'block';
+        button.innerText = translations[currentLanguage]["hideTasks"];
+    }
+}
+
